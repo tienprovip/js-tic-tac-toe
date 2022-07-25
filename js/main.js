bc63863dcebc41d98e489d4fca22c01d02e69636
Ptien1305 <ptien1305@gmail.com>
2022-07-25 16:48:45 +0700

    add selector

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+import { getCellElementAtIdx, getCellElementList, getCurrentTurnElement, getGameStatusElement } from "./selectors.js";
+
+console.log(getCellElementList());
+console.log(getCurrentTurnElement());
+console.log(getGameStatusElement());
+console.log(getCellElementAtIdx(3));
+
 /**
  * Global variables
  */
